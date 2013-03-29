5cc0525757844cf449ec6a941ad090b23671c348
Wilfred Hughes <me@wilfred.me.uk>
2013-03-29 22:52:45 +0000

    initial commit with skeleton code

---
+++ re.el
@@ -0,0 +1,23 @@
+;;; re.el --- a friendly regexp library
+
+;;; Commentary
+
+;; I find Emacs' built-in regexp library tricky to use. Maybe you do
+;; too.
+
+;; re.el provides functions that just return a match string rather
+;; than using several different variables for all the different match
+;; details.
+
+;;; search functions
+(defun re-find (pattern string))
+(defun re-find-all (pattern string))
+
+;;; match metadata
+(defun re-match-text (match))
+(defun re-match-start (match))
+(defun re-match-end (match))
+
+;;; miscellaneous
+(defun re-strip-properties (string))
+(defun re-from-pcre (pattern))
