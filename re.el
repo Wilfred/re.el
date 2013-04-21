5b3e8afcdd13bb84dad53379501b4bd253545011
Wilfred Hughes <me@wilfred.me.uk>
2013-04-21 20:34:02 +0100

    adding license

--- re.el
+++ re.el
@@ -1,6 +1,25 @@
 ;;; re.el --- a friendly regexp library
 
-;;; Commentary
+;; Copyright (C) 2013 Wilfred Hughes
+
+;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Version: 0.1
+;; Keywords: regexp, regular expression
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
 
 ;; I find Emacs' built-in regexp library tricky to use. Maybe you do
 ;; too.
@@ -8,6 +27,8 @@
 ;; re.el provides functions that just return a match string rather
 ;; than using several different variables for all the different match
 ;; details.
+
+;;; Code:
 
 ;;; search functions
 (defun re-find (string pattern &optional ignore-case))
